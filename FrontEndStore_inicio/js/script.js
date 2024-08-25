0adb075788223564a99848fc0001ebc3ec79d6cc
xxpalinxx <144467903+xxpalinxx@users.noreply.github.com>
2024-08-25 11:22:45 -0300

    agregado fetch
    Actualizado el codigo con el uso de fetch y creacion de archivo info.json para el array de productos en formato JSON

--- FrontEndStore_inicio/js/script.js
+++ FrontEndStore_inicio/js/script.js
@@ -1,19 +1,7 @@
-let remeras = [
-    {id: 1, img:"./img/1.jpg" , nombre: "vuejs", precio: 25, color: "gris"},
-    {id: 2, img:"./img/2.jpg" , nombre: "angularjs", precio: 25, color: "gris"},
-    {id: 3, img:"./img/3.jpg" , nombre: "react", precio: 40, color: "negro"},
-    {id: 4, img:"./img/4.jpg" , nombre: "redux", precio: 25, color: "amarillo"},
-    {id: 5, img:"./img/5.jpg" , nombre: "nodejs", precio: 35, color: "gris"},
-    {id: 6, img:"./img/6.jpg" , nombre: "sass", precio: 25, color: "negro"},
-    {id: 7, img:"./img/7.jpg" , nombre: "html5", precio: 15, color: "gris"},
-    {id: 8, img:"./img/8.jpg" , nombre: "github", precio: 10, color: "violeta"},
-    {id: 9, img:"./img/9.jpg" , nombre: "bulma", precio: 25, color: "rojo"},
-    {id: 10, img:"./img/10.jpg" , nombre: "typescript", precio: 35, color: "blanco"},
-    {id: 11, img:"./img/11.jpg" , nombre: "drupal", precio: 25, color: "azul"},
-    {id: 12, img:"./img/12.jpg" , nombre: "javascript", precio: 45, color: "amarillo"},
-    {id: 13, img:"./img/13.jpg" , nombre: "graphql", precio: 25, color: "negro"},
-    {id: 14, img:"./img/14.jpg" , nombre: "wordpress", precio: 25, color: "rojo"}
-]
+fetch('./js/info.json')
+    .then(respuesta => respuesta.json())
+    .then(remeras => main(remeras))
+    .catch(error => console.log(error))
 
 /**----------------------------------------------------PRODUCTOS---------------------------------------- */
 function crearTarjetasProductos(remeras) {
@@ -154,12 +142,12 @@ function renderizarCarrito(carrito) {
 
         let botonRestar = document.getElementById(`br${producto.id}`)
         botonRestar.addEventListener("click", (e) => restarUnidad(e))
-    });
+    })
 }
 
 
 
-/*---------------------------------------------------BOTONES-------------------------------------------*/
+/*----------------------------------BOTONES SUMAR, RESTAR, ELIMINAR-------------------------------------------*/
 function sumarUnidad(e) {
     let id = Number(e.target.id.substring(2))
     let carrito = obtenerCarrito()
@@ -210,6 +198,7 @@ function eliminarProductoCarrito(e) {
     alertaToast('Producto eliminado del carrito')
 }
 
+/**-----------------------------------CALCULAR Y MOSTRAR TOTAL---------------------------------------------- */
 function calcularTotal(carrito) {
     let total = carrito.reduce((acum, producto) => acum + producto.subtotal, 0)
     localStorage.setItem("total", total)
@@ -324,5 +313,3 @@ function main(remeras){
     let contadorCarritoCompras = document.getElementById("cart")
     contadorCarritoCompras.addEventListener("click", mostrarOcultarCarrito)
 }
-
-main(remeras)
